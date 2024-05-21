6226bbdd094dddb9ac78109a80b4544deeb15b72
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-21 18:34:07 +0600

    successfully created product post operation

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express'
 import { ProductRoutes } from './modules/products/products.route'
 const app = express()
+// parser
+app.use(express.json())
 
 app.use("/api/products" ,ProductRoutes)
 
--- src/modules/products/products.route.ts
+++ src/modules/products/products.route.ts
@@ -1,9 +1,16 @@
 import express, { Request, Response } from "express"
+import { Product } from "./products.model";
 
 const router = express.Router()
 
-router.post("/",(req:Request , res:Response) =>{
-    res.send("hi")
+router.post("/",async(req:Request , res:Response) =>{
+   
+    const result = await Product.create(req.body);
+    res.json({
+        success: true,
+        message: "Product is added successfully",
+        data: result
+    })
 })
 
 export const ProductRoutes = router;
